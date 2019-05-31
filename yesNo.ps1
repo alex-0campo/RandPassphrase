bd91ed611180fbe2189f27a3c91381b38cc98ef1
alex-0campo <alexo@landesa.org>
2019-05-30 21:42:30 -0700

    Initial commit yesNo.ps1 loop example

---
+++ yesNo.ps1
@@ -0,0 +1,21 @@
+﻿Clear-Host
+
+[bool]$stopExecution = $false
+
+do {
+    $reply = Read-Host "Do you want to stop? yes/no"
+
+    if ( $reply.ToUpper() -eq "YES" )
+    {
+        Write-Host "You said STOP; closing in 5-seconds."
+        $stopExecution = $true
+        Start-Sleep -Seconds 5
+        exit
+    }
+    else
+    {
+        Write-Host "You entered no."  
+    }
+
+    Write-Host "Repeat loop..."
+} while ( $stopExecution -eq $false)
